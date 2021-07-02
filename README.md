abd64ced8aa8aeeec12c769ec9009df38629d778
CrysMello <crystianemello@hotmail.com>
2021-07-02 00:13:20 -0300

    atualizando readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# projeto-final-reprograma
+O projeto foi criado para ajudar pessoas que gostam de praticar canicrros, mas precisam de um local adequado para a pratica e muitas vezes para se hospedar com o seu cachorro.
+
+
 Está aplicação é uma API que tem como foco o cadastramento de estabelecimentos  que tenham sugestões de lugares e hospedagem próximos para  a prática de Canicross.
--- src/routes/establishmentRoutes.js
+++ src/routes/establishmentRoutes.js
@@ -2,13 +2,12 @@ const express = require("express");
 const controller = require("../controllers/establishmentController");
 const router = express.Router();
 
+router.post("/register", controller.createEstablishment);
 
-router.post("/register", controller.createEstablishment)
+router.get("/register", controller.showEstablishments);
 
-router.get("/register", controller.showEstablishments)
+router.patch("/register/:nome", controller.updateStablishment);
 
-router.patch("/register/:id", controller.updateStablishment);
+router.delete("/register/:nome", controller.deleteEstablishiment);
 
-router.delete("/register/:id", controller.deleteEstablishiment)
-
-module.exports = router
+module.exports = router;
